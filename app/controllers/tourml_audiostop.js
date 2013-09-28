6ecd13e531769c8e4cc47a53297e99b2d40972f3
gautiermichelin <gm@ideesculture.com>
2013-09-28 21:59:53 +0200

    adding an audio player with specific handling for android
    Signed-off-by: gautiermichelin <gm@ideesculture.com>

--- app/controllers/tourml_audiostop.js
+++ app/controllers/tourml_audiostop.js
@@ -8,6 +8,10 @@ var CONFIG = arguments[0] || {};
 var ACTION = {};
 var STREAM;
 
+var DURATION;
+var PAUSED = 0;
+var RESTART_TIME;
+
 $.init = function() {
 	APP.log("debug", "tourml_stop.init | " + JSON.stringify(CONFIG));
 
@@ -38,6 +42,14 @@ $.handleData = function(_data) {
 		subdata = _data.subdata;
 		image_file = _data.image;
 		audio_file = _data.audio;
+		if(_data.duration) {
+			var _duration = _data.duration.split(":");
+			APP.log("debug", "tourml_audiostop._duration | " + JSON.stringify(_duration));
+			DURATION = ((_duration[0] * 60 + _duration[1]) * 60 + _duration[2]) * 1000;
+			APP.log("debug", "tourml_audiostop.DURATION | " + JSON.stringify(DURATION));
+		} else {
+			DURATION = 0;
+		}
 	} else {
 		$.title.text = "Problem";
 	}
@@ -74,25 +86,38 @@ $.handleData = function(_data) {
 };
 
 $.createAudioPlayer = function(_url) {
-	APP.log("debug", "podcast_podcast.createAudioPlayer(" + _url + ")");
+	APP.log("debug", "tourml_audiostop.createAudioPlayer(" + _url + ")");
 
 	Ti.Media.audioSessionMode = Ti.Media.AUDIO_SESSION_MODE_PLAYBACK;
 
-	STREAM = Ti.Media.createVideoPlayer({
-		url: _url,
-		backgroundColor: "#000",
-		fullscreen: false,
-		allowsAirPlay: true,
-		mediaControlStyle: Ti.Media.VIDEO_CONTROL_NONE,
-		mediaTypes: Ti.Media.VIDEO_MEDIA_TYPE_AUDIO,
-		repeatMode: Ti.Media.VIDEO_REPEAT_MODE_NONE,
-		sourceType: Ti.Media.VIDEO_SOURCE_TYPE_STREAMING,
-		useApplicationAudioSession: true,
-		visible: false
-	});
+	var soundURL = Titanium.Filesystem.getFile(Titanium.Filesystem.resourcesDirectory, _url).nativePath;
 
-	STREAM.addEventListener("playbackstate", $.streamState);
-	STREAM.addEventListener("loadstate", $.streamPlay);
+	if(OS_IOS) {
+		STREAM = Ti.Media.createVideoPlayer({
+			url: _url,
+			backgroundColor: "#000",
+			fullscreen: false,
+			allowsAirPlay: true,
+			mediaControlStyle: Ti.Media.VIDEO_CONTROL_NONE,
+			mediaTypes: Ti.Media.VIDEO_MEDIA_TYPE_AUDIO,
+			repeatMode: Ti.Media.VIDEO_REPEAT_MODE_NONE,
+			sourceType: Ti.Media.VIDEO_SOURCE_TYPE_STREAMING,
+			useApplicationAudioSession: true,
+			visible: false
+		});
+
+		STREAM.addEventListener("playbackstate", $.streamState);
+		STREAM.addEventListener("loadstate", $.streamPlay);
+	}
+	if(OS_ANDROID) {
+		STREAM = Titanium.Media.createAudioPlayer({});
+		STREAM.addEventListener("loadstate", $.streamPlay);
+		STREAM.setUrl(soundURL);
+
+		$.streamPlay();
+
+		RESTART_TIME = new Date().getTime();
+	}
 
 	setInterval($.streamProgress, 500);
 };
@@ -107,7 +132,7 @@ $.handleNavigation = function(_id) {
 		navmap: function(_event) {
 			STREAM.stop();
 			STREAM.release();
-			APP.log("debug", "tourml @map");
+			APP.log("debug", "tourml_audio @map");
 
 			APP.addChild("tourml_map2", {
 				index: CONFIG.index,
@@ -117,7 +142,7 @@ $.handleNavigation = function(_id) {
 		navkeypad: function(_event) {
 			STREAM.stop();
 			STREAM.release();
-			APP.log("debug", "tourml @keypad");
+			APP.log("debug", "tourml_audio @keypad");
 
 			APP.addChild("tourml_keypad", {
 				index: CONFIG.index,
@@ -131,39 +156,76 @@ $.handleNavigation = function(_id) {
 
 $.streamPlay = function(_event) {
 	STREAM.play();
+	$.play.visible = false;
+	$.pause.visible = true;
+	RESTART_TIME = new Date().getTime();
 };
 
 $.streamPause = function(_event) {
+	$.pause.visible = false;
+	$.play.visible = true;
 	STREAM.pause();
+	var newTime = new Date().getTime();
+	PAUSED = PAUSED + newTime - RESTART_TIME;
 };
 
 $.streamStop = function() {
+	$.pause.visible = false;
+	$.play.visible = true;
 	STREAM.stop();
 	STREAM.release();
 };
 
-$.streamSeek = function(_event) {
-	var x = _event.x;
-	var width = $.track.rect.width;
-	var percentage = x / width;
-	var position = Math.round(STREAM.getDuration() * percentage);
+if(OS_IOS) {
+	$.streamSeek = function(_event) {
+		var x = _event.x;
+		var width = $.track.rect.width;
+		var percentage = x / width;
+		var position = Math.round(STREAM.getDuration() * percentage);
 
-	STREAM.setCurrentPlaybackTime(position);
+		APP.log("debug", "streamseek " + STREAM.getDuration() + " " + x);
 
-	$.position.width = (percentage * 100) + "%";
-};
+		STREAM.setCurrentPlaybackTime(position);
 
-$.streamProgress = function(_event) {
-	if(STREAM.playbackState == Ti.Media.VIDEO_PLAYBACK_STATE_PLAYING) {
-		var percentage = ((STREAM.currentPlaybackTime / STREAM.getDuration()) * 100);
-		var time = DATE.duration(STREAM.currentPlaybackTime);
+		$.position.width = (percentage * 100) + "%";
+	};
+}
 
-		percentage = percentage >= 1 ? percentage : 1;
+if(OS_IOS) {
+	$.streamProgress = function(_event) {
+		if(STREAM.playbackState == Ti.Media.VIDEO_PLAYBACK_STATE_PLAYING) {
+			var percentage = ((STREAM.currentPlaybackTime / STREAM.getDuration()) * 100);
+			var time = DATE.duration(STREAM.currentPlaybackTime);
 
-		$.position.width = percentage + "%";
-		$.time.text = (time.hours() !== 0 ? time.hours() + ":" : "") + time.minutes() + ":" + (time.seconds() < 10 ? "0" : "") + time.seconds();
-	}
-};
+			percentage = percentage >= 1 ? percentage : 1;
+
+			$.position.width = percentage + "%";
+			$.time.text = (time.hours() !== 0 ? time.hours() + ":" : "") + time.minutes() + ":" + (time.seconds() < 10 ? "0" : "") + time.seconds();
+		}
+	};
+}
+
+if(OS_ANDROID) {
+	$.streamProgress = function(_event) {
+		if(STREAM.isPlaying() && !STREAM.isPaused()) {
+			var newTime = new Date().getTime();
+			var _progress = PAUSED + newTime - RESTART_TIME;
+		}
+		if(STREAM.isPlaying() && (DURATION > 0)) {
+			APP.log("debug", "_progress " + _progress);
+			var percentage = ((_progress / DURATION) * 100);
+			percentage = percentage >= 1 ? percentage : 1;
+			$.position.width = percentage + "%";
+
+			var seconds = Math.floor((_progress / 1000) % 60);
+			APP.log("debug", "seconds " + seconds);
+			var minutes = Math.floor((_progress / (1000 * 60)) % 60);
+			APP.log("debug", "minutes " + minutes);
+			var hours = Math.floor((_progress / (1000 * 60 * 60)) % 24);
+			$.time.text = (hours !== 0 ? hours + ":" : "") + minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+		}
+	};
+}
 
 $.streamState = function(_event) {
 	if(_event.playbackState == Ti.Media.VIDEO_PLAYBACK_STATE_PLAYING) {
@@ -178,7 +240,9 @@ $.streamState = function(_event) {
 // Event listeners
 $.play.addEventListener("click", $.streamPlay);
 $.pause.addEventListener("click", $.streamPause);
-$.track.addEventListener("click", $.streamSeek);
+if(OS_IOS) {
+	$.track.addEventListener("click", $.streamSeek);
+}
 
 // Kick off the init
 $.init();
