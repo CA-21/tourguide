bd37826842b413febb559507ea366b3b88027d88
gautiermichelin <gm@ideesculture.com>
2013-12-08 08:33:56 +0100

    not needed, removing
    Signed-off-by: gautiermichelin <gm@ideesculture.com>

--- compile-install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ~/CloudStation/Titanium/workspace/TourGuide
-#Alloy compile --config platform=android
-titanium build --platform=android --build-only
-adb -s 192.168.56.101:5555 shell pm uninstall io.tourguide
-adb -s 192.168.56.101:5555 install -r build/android/bin/app.apk
